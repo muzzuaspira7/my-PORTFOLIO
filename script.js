8923eee7f28736c632b3a301cd43ce7d14513e3c
muzzuaspira7 <muzzuaspira7@gmail.com>
2023-10-18 22:02:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = new Typed('#changing-text', {
+    strings: ['Web Developer', 'Web Designer', '.Net Developer', 'Web App Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });
