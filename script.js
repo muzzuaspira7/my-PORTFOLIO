66ac7d41791898d2c25755118d958960a24f2209
muzzuaspira7 <muzzuaspira7@gmail.com>
2023-10-23 22:00:53 +0530

    last

--- script.js
+++ script.js
@@ -4,63 +4,63 @@ const typed = new Typed('#changing-text', {
   backSpeed: 50,
   loop: true
 });
-// Nav scroll
 
-    // Function to add or remove the 'active' class based on scroll position
-    function setActiveNav() {
-      const sections = document.querySelectorAll("section"); // Replace with your section elements
-      const navLinks = document.querySelectorAll(".navbar a, .bottom-icons a i");
+// Projects
 
-      sections.forEach((section, index) => {
-          const rect = section.getBoundingClientRect();
-          if (rect.top <= 150 && rect.bottom >= 150) {
-              // Highlight the corresponding nav link
-              navLinks[index].classList.add("active");
-          } else {
-              // Remove the 'active' class from nav link
-              navLinks[index].classList.remove("active");
-          }
+  document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+      card.addEventListener("click", function () {
+        card.classList.toggle("flipped");
       });
-  }
+    });
+  });
 
-  // Listen for scroll events and call the setActiveNav function
-  window.addEventListener("scroll", setActiveNav);
 
-  // Call setActiveNav initially to highlight the active section on page load
-  setActiveNav();
+
+// Function to add or remove the 'active' class based on scroll position
+function setActiveNav() {
+  const sections = document.querySelectorAll("section"); // Replace with your section elements
+  const navLinks = document.querySelectorAll(".navbar a");
+  const icons = document.querySelectorAll(".bottom-icons a i");
+
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 150 && rect.bottom >= 150) {
+      // Highlight the corresponding nav link and icon
+      navLinks[index].classList.add("active");
+      icons[index].classList.add("active");
+    } else {
+      // Remove the 'active' class from nav link and icon
+      navLinks[index].classList.remove("active");
+      icons[index].classList.remove("active");
+    }
+  });
+}
+
+// Listen for scroll events and call the setActiveNav function
+window.addEventListener("scroll", setActiveNav);
+
+// Call setActiveNav initially to highlight the active section on page load
+setActiveNav();
+
 
 
   
-// // TILT
-const cards = document.querySelectorAll('.tilt');
-for (let i = 0; i < cards.length; i++) {
-  const card = cards[i];
-  card.addEventListener('mousemove', rotate);
-  card.addEventListener('mouseout', stopRotate)
-}
-
-function rotate(e) {
-  const cardItem = this.querySelector('.card-item');
-  const halfHeight = cardItem.offsetHeight / 2;
-
-  cardItem.style.transform = 'rotateX(' + -(e.offsetY - halfHeight) / 7 + 'deg) rotateY(' + (e.offsetX - halfHeight) / 7 + 'deg)';
-}
-function stopRotate() {
-  const cardItem = this.querySelector('.hero-img');
-  cardItem.style.transform = 'rotate(0)';
-}
-console.log(cards);
 // TIME LINE
 (function () {
   "use strict";
 
   // define variables
+  
   var items = document.querySelectorAll(".timeline li");
 
   // check if an element is in viewport
   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
+    
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
@@ -84,6 +84,8 @@ console.log(cards);
   window.addEventListener("scroll", callbackFunc);
 })();
 
+
+
 // Contact
 // document.getElementsByClassName('successfully-saved')
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwvlmFmTZeDsL3mqALhcx51fnu_p6lzzC19uurAjn6hvcNdhY_BSzn4DlE3lKmQYFY_iA/exec'
@@ -96,3 +98,21 @@ form.addEventListener('submit', e => {
             console.log('Success!', response))
         .catch(error => console.error('Error!', error.message))
 })
+
+
+// Swipper
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+      rotate: 15,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: true,
+  },
+  loop: true,
+});
+
